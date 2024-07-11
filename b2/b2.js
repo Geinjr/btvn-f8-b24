40ee15c912b0c3f7d566791a3ebb07f8d5a8719d
Geinjr <dokienktkt@gmail.com>
2024-07-11 18:56:28 +0700

    update

--- b2/b2.js
+++ b2/b2.js
@@ -1,9 +1,16 @@
-var nums1 = [1, 2];
+var nums1 = [1, 7, 2];
 var nums2 = [3, 4, 5];
 
 var mergedArray = nums1.concat(nums2);
 var lengthOfMergedArray = mergedArray.length;
-let result;
+
+function sortAscending(arr) {
+    return arr.sort((a, b) => a - b);
+}
+
+mergedArray = sortAscending(mergedArray);
+
+var result;
 
 if (lengthOfMergedArray % 2 === 0) {
     result =
